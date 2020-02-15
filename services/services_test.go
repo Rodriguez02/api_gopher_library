0ff4d7e8cf33fc0e2da71ef116ffa046fe957cc0
emicalvacho <emilianocalvacho@gmail.com>
2020-02-15 17:31:28 -0300

    Finish testing

--- services/services_test.go
+++ services/services_test.go
@@ -5,85 +5,88 @@ import (
 	"testing"
 )
 
-/************************** TEST LOANS ************************/
-
-/* create loan */
-
-func TestCreateLoanNotOk(t *testing.T) {
-	r := []struct {
-		TestName string
-		Loan     domain.Loan
-		Expected error
-	}{
-		{
-			TestName: "loan exists",
-			Loan: domain.Loan{
-				ID:      1,
-				IDBook:  "1C",
-				IDUser:  1,
-				DueDate: "2020-03-12",
-				Info:    domain.Information{},
-			},
-			Expected: ErrorLoanExists,
-		},
-		{
-			TestName: "incorrect id book",
-			Loan: domain.Loan{
-				ID:      2,
-				IDBook:  "",
-				IDUser:  1,
-				DueDate: "2020-03-12",
-				Info:    domain.Information{},
-			},
-			Expected: ErrorNoIDBook,
-		},
-		{
-			TestName: "incorrect id user",
-			Loan: domain.Loan{
-				ID:      2,
-				IDBook:  "aaaa",
-				IDUser:  0,
-				DueDate: "2020-03-12",
-				Info:    domain.Information{},
-			},
-			Expected: ErrorNoIDUser,
-		},
-	}
-
-	l := domain.Loan{
+var (
+	requestLoan = domain.Loan{
 		ID:      1,
-		IDBook:  "1B",
+		IDBook:  "1C",
 		IDUser:  1,
 		DueDate: "2020-03-12",
 		Info:    domain.Information{},
 	}
-	loans = append(loans, l)
+	expectedLoan = domain.Loan{
+		ID:      1,
+		IDBook:  "1C",
+		IDUser:  1,
+		DueDate: "2020-03-12",
+		Info: domain.Information{
+			Titulo:           "The City",
+			Subtitulo:        "",
+			Autores:          [5]string{"Robert E. Park", "Ernest W. Burgess", "", "", ""},
+			FechaPublicacion: "2019-04-19",
+		},
+	}
+	expectedUser = domain.User{
+		ID:       1,
+		Nombre:   "Nombre",
+		Apellido: "Apellido",
+	}
+	url = based + "Lord" + "+inauthor:" + "tolkien" + "&fields=items(id,volumeInfo(title,subtitle,authors,publishedDate))"
+)
 
-	for _, c := range r {
-		t.Run(c.TestName, func(t *testing.T) {
-			_, err := CreateLoan(c.Loan)
-			if c.Expected != err {
-				t.Fail()
-			}
-		})
+func ClearArrays() {
+	users = []domain.User{}
+	loans = []domain.Loan{}
+}
+
+func CreateDataUser() {
+	CreateUser(domain.User{
+		ID:       1,
+		Nombre:   "Nombre",
+		Apellido: "Apellido",
+	})
+}
+
+/************************** TEST LOANS ************************/
+
+/* create loan */
+func TestCreateLoanOk(t *testing.T) {
+	ClearArrays()
+	CreateDataUser()
+
+	l, _ := CreateLoan(requestLoan)
+	if l != expectedLoan {
+		t.Fail()
+	}
+}
+
+func TestCreateLoanExist(t *testing.T) {
+	ClearArrays()
+	CreateDataUser()
+	loans = append(loans, expectedLoan)
+
+	_, err := CreateLoan(requestLoan)
+	if err != ErrorLoanExists {
+		t.Fail()
+	}
+}
+
+func TestCreateLoanNoAvailability(t *testing.T) {
+	ClearArrays()
+	CreateDataUser()
+	booksAmount["1C"] = 5
+
+	_, err := CreateLoan(requestLoan)
+	if err != ErrorNoAvailability {
+		t.Fail()
 	}
 }
 
 /* get loan */
 
 func TestGetAllLoansOk(t *testing.T) {
-	CreateUser(domain.User{
-		ID:       12,
-		Nombre:   "Nombre",
-		Apellido: "Apellido",
-	})
-	CreateLoan(domain.Loan{
-		ID:      12,
-		IDBook:  "1B",
-		IDUser:  12,
-		DueDate: "2020-03-12",
-		Info:    domain.Information{},
-	})
+	ClearArrays()
+	CreateDataUser()
+	loans = append(loans, expectedLoan)
 
 	_, err := GetAllLoans()
 	if err == ErrorLoansNotFound {
@@ -92,7 +95,8 @@ func TestGetAllLoansOk(t *testing.T) {
 }
 
 func TestGetAllLoansNotOk(t *testing.T) {
-	loans = []domain.Loan{}
+	ClearArrays()
+
 	_, err := GetAllLoans()
 	if err != ErrorLoansNotFound {
 		t.Fail()
@@ -100,27 +104,19 @@ func TestGetAllLoansNotOk(t *testing.T) {
 }
 
 func TestGetLoanOk(t *testing.T) {
-	CreateUser(domain.User{
-		ID:       10,
-		Nombre:   "Nombre",
-		Apellido: "Apellido",
-	})
-	CreateLoan(domain.Loan{
-		ID:      10,
-		IDBook:  "1B",
-		IDUser:  10,
-		DueDate: "2020-03-12",
-		Info:    domain.Information{},
-	})
+	ClearArrays()
+	CreateDataUser()
+	loans = append(loans, expectedLoan)
 
-	id := "10"
-	_, err := GetLoan(id)
+	_, err := GetLoan("1")
 	if err != nil {
 		t.Fail()
 	}
 }
 
 func TestGetLoanNotOk(t *testing.T) {
+	ClearArrays()
+
 	r := []struct {
 		TestName string
 		IdLoan   string
@@ -151,22 +147,14 @@ func TestGetLoanNotOk(t *testing.T) {
 /* update loan */
 
 func TestUpdateLoanOk(t *testing.T) {
-	CreateUser(domain.User{
-		ID:       20,
-		Nombre:   "Nombre",
-		Apellido: "Apellido",
-	})
-	CreateLoan(domain.Loan{
-		ID:      20,
-		IDBook:  "1B",
-		IDUser:  20,
-		DueDate: "2020-03-12",
-		Info:    domain.Information{},
-	})
+	ClearArrays()
+	CreateDataUser()
+	loans = append(loans, expectedLoan)
+
 	_, err := UpdateLoan(domain.Loan{
-		ID:      20,
+		ID:      1,
 		IDBook:  "1R",
-		IDUser:  20,
+		IDUser:  1,
 		DueDate: "2020-03-12",
 		Info:    domain.Information{},
 	})
@@ -175,105 +163,32 @@ func TestUpdateLoanOk(t *testing.T) {
 	}
 }
 
-func TestUpdateLoanNotOk(t *testing.T) {
-	CreateUser(domain.User{
-		ID:       20,
-		Nombre:   "Nombre",
-		Apellido: "Apellido",
-	})
-	CreateLoan(domain.Loan{
-		ID:      20,
-		IDBook:  "1B",
-		IDUser:  20,
-		DueDate: "2020-03-12",
-		Info:    domain.Information{},
-	})
-	
-	r := []struct {
-		TestName string
-		Loan     domain.Loan
-		Expected error
-	}{
-		{
-			TestName: "Loan not exist",
-			Loan: domain.Loan{
-				ID:      1000,
-				IDBook:  "1R",
-				IDUser:  1000,
-				DueDate: "2020-03-12",
-				Info:    domain.Information{},
-			},
-			Expected: ErrorLoanNotFound,
-		},
-		{
-			TestName: "user not exist",
-			Loan: domain.Loan{
-				ID:      20,
-				IDBook:  "jh",
-				IDUser:  9,
-				DueDate: "2020-03-12",
-				Info:    domain.Information{},
-			},
-			Expected: ErrorUserNotFound,
-		},
-		{
-			TestName: "id book incorrect",
-			Loan: domain.Loan{
-				ID:      20,
-				IDBook:  "",
-				IDUser:  20,
-				DueDate: "2020-03-12",
-				Info:    domain.Information{},
-			},
-			Expected: ErrorNoIDBook,
-		},
-		{
-			TestName: "id user incorrect",
-			Loan: domain.Loan{
-				ID:      20,
-				IDBook:  "ab",
-				IDUser:  0,
-				DueDate: "2020-03-12",
-				Info:    domain.Information{},
-			},
-			Expected: ErrorNoIDUser,
-		},
-	}
+func TestUpdateLoanNotFound(t *testing.T) {
+	ClearArrays()
+	CreateDataUser()
 
-	for _, c := range r {
-		t.Run(c.TestName, func(t *testing.T) {
-			_, err := UpdateLoan(c.Loan)
-			if c.Expected != err {
-				t.Fail()
-			}
-		})
+	_, err := UpdateLoan(expectedLoan)
+	if err != ErrorLoanNotFound {
+		t.Fail()
 	}
 }
 
 /* delete loan */
 
 func TestDeleteLoanOk(t *testing.T) {
-	CreateUser(domain.User{
-		ID:       10,
-		Nombre:   "Nombre",
-		Apellido: "Apellido",
-	})
-	CreateLoan(domain.Loan{
-		ID:      10,
-		IDBook:  "1B",
-		IDUser:  10,
-		DueDate: "2020-03-12",
-		Info:    domain.Information{},
-	})
+	ClearArrays()
+	CreateDataUser()
+	loans = append(loans, expectedLoan)
 
-	id := "10"
-	_, err := DeleteLoan(id)
+	_, err := DeleteLoan("1")
 	if err != nil {
 		t.Fail()
 	}
 }
 
 func TestDeleteLoanNotOk(t *testing.T) {
+	ClearArrays()
+
 	r := []struct {
 		TestName string
 		IdLoan   string
@@ -305,32 +220,18 @@ func TestDeleteLoanNotOk(t *testing.T) {
 
 /* create users */
 func TestCreateUserOK(t *testing.T) {
-	r := struct {
-		TestName string
-		User     domain.User
-		Expected domain.User
-	}{
-		TestName: "Correct user",
-		User: domain.User{
-			ID:       1,
-			Nombre:   "Nombre",
-			Apellido: "Apellido",
-		},
-		Expected: domain.User{
-			ID:       1,
-			Nombre:   "Nombre",
-			Apellido: "Apellido",
-		},
+	ClearArrays()
+
+	r, _ := CreateUser(expectedUser)
+	if r != expectedUser {
+		t.Fail()
 	}
-	t.Run(r.TestName, func(t *testing.T) {
-		result, _ := CreateUser(r.User)
-		if r.Expected != result {
-			t.Fail()
-		}
-	})
 }
 
 func TestCreateUserNotOk(t *testing.T) {
+	ClearArrays()
+	CreateDataUser()
+
 	r := []struct {
 		TestName string
 		User     domain.User
@@ -376,12 +277,6 @@ func TestCreateUserNotOk(t *testing.T) {
 
 	for _, c := range r {
 		t.Run(c.TestName, func(t *testing.T) {
-			u := domain.User{
-				ID:       1,
-				Nombre:   "Nombre",
-				Apellido: "Apellido",
-			}
-			users = append(users, u)
 			_, err := CreateUser(c.User)
 			if c.Expected != err {
 				t.Fail()
@@ -393,11 +288,9 @@ func TestCreateUserNotOk(t *testing.T) {
 /* get users */
 
 func TestGetAllUsersOK(t *testing.T) {
-	CreateUser(domain.User{
-		ID:       2,
-		Nombre:   "Nombre",
-		Apellido: "Apellido",
-	})
+	ClearArrays()
+	CreateDataUser()
+
 	_, err := GetAllUsers()
 	if ErrorUsersNotFound == err {
 		t.Fail()
@@ -405,7 +298,8 @@ func TestGetAllUsersOK(t *testing.T) {
 }
 
 func TestGetAllUsersNotOK(t *testing.T) {
-	users = []domain.User{}
+	ClearArrays()
+
 	_, err := GetAllUsers()
 	if ErrorUsersNotFound != err {
 		t.Fail()
@@ -413,189 +307,213 @@ func TestGetAllUsersNotOK(t *testing.T) {
 }
 
 func TestGetUserOK(t *testing.T) {
-	r := struct {
-		TestName string
-		User     domain.User
-		Expected error
-	}{
-		TestName: "Get user",
-		User: domain.User{
-			ID:       1,
-			Nombre:   "Nombre",
-			Apellido: "Apellido",
-		},
-		Expected: nil,
-	}
+	ClearArrays()
+	CreateDataUser()
 
-	t.Run(r.TestName, func(t *testing.T) {
-		CreateUser(r.User)
-		id := "1"
-		_, err := GetUser(id)
-		if r.Expected != err {
-			t.Fail()
-		}
-	})
+	u, _ := GetUser("1")
+	if u != expectedUser {
+		t.Fail()
+	}
 }
 
 func TestGetUserNotOK(t *testing.T) {
-	r := struct {
-		TestName string
-		User     domain.User
-		Expected error
-	}{
-		TestName: "Get all users",
-		User: domain.User{
-			ID:       1,
-			Nombre:   "Nombre",
-			Apellido: "Apellido",
-		},
-		Expected: ErrorUserNotFound,
-	}
+	ClearArrays()
+	CreateDataUser()
 
-	t.Run(r.TestName, func(t *testing.T) {
-		CreateUser(r.User)
-		id := "2"
-		_, err := GetUser(id)
-		if r.Expected != err {
-			t.Fail()
-		}
-	})
+	_, err := GetUser("2")
+	if ErrorUserNotFound != err {
+		t.Fail()
+	}
 }
 
 /* update users */
 
 func TestUpdateUserOK(t *testing.T) {
-	r := struct {
-		TestName string
-		User     domain.User
-		Expected error
-	}{
-		TestName: "Update user",
-		User: domain.User{
-			ID:       1,
-			Nombre:   "Nombre",
-			Apellido: "Apellido",
-		},
-		Expected: nil,
-	}
+	ClearArrays()
+	CreateDataUser()
 
-	t.Run(r.TestName, func(t *testing.T) {
-		u := domain.User{
-			ID:       1,
-			Nombre:   "Nombre1",
-			Apellido: "Apellido1",
-		}
-		CreateUser(r.User)
-		_, err := UpdateUser(u)
-		if r.Expected != err {
-			t.Fail()
-		}
-
+	_, err := UpdateUser(domain.User{
+		ID:       1,
+		Nombre:   "Nombre1",
+		Apellido: "Apellido1",
 	})
-}
-
-func TestUpdateUserNotOK(t *testing.T) {
-	r := struct {
-		TestName string
-		User     domain.User
-		Expected error
-	}{
-		TestName: "No update user",
-		User: domain.User{
-			ID:       1,
-			Nombre:   "Nombre",
-			Apellido: "Apellido",
-		},
-		Expected: ErrorUserNotFound,
-	}
-
-	t.Run(r.TestName, func(t *testing.T) {
-		u := domain.User{
-			ID:       2,
-			Nombre:   "Nombre1",
-			Apellido: "Apellido1",
-		}
-		CreateUser(r.User)
-		_, err := UpdateUser(u)
-		if r.Expected != err {
-			t.Fail()
-		}
-
-	})
-}
-
-/* delete users */
-
-func TestDeleteUserOk(t *testing.T) {
-	r := struct {
-		TestName string
-		User     domain.User
-		Expected error
-	}{
-		TestName: "Delete user",
-		User: domain.User{
-			ID:       1,
-			Nombre:   "Nombre",
-			Apellido: "Apellido",
-		},
-		Expected: nil,
-	}
-
-	t.Run(r.TestName, func(t *testing.T) {
-		CreateUser(r.User)
-		id := "1"
-		_, err := DeleteUser(id)
-		if r.Expected != err {
-			t.Fail()
-		}
-	})
-}
-
-func TestDeleteUserNotOk(t *testing.T) {
-	r := struct {
-		TestName string
-		User     domain.User
-		Expected error
-	}{
-		TestName: "Delete user failed",
-		User: domain.User{
-			ID:       1,
-			Nombre:   "Nombre",
-			Apellido: "Apellido",
-		},
-		Expected: ErrorUserNotFound,
-	}
-
-	t.Run(r.TestName, func(t *testing.T) {
-		CreateUser(r.User)
-		id := "2"
-		_, err := DeleteUser(id)
-		if r.Expected != err {
-			t.Fail()
-		}
-	})
-}
-
-/************************** TEST BOOKS ************************/
-
-func TestValidateBookOk(t *testing.T) {
-	book := domain.Book{
-		Titulo: "Titulo",
-		Autor:  "Autor",
-	}
-	err := validateBook(book)
 	if err != nil {
 		t.Fail()
 	}
 }
 
-func TestValidateBookNotOk(t *testing.T) {
-	book := domain.Book{
-		Titulo: "Titu/lo",
-		Autor:  "Autor",
+func TestUpdateUserNotOK(t *testing.T) {
+	ClearArrays()
+	CreateDataUser()
+
+	r := []struct {
+		TestName string
+		User     domain.User
+		Expected error
+	}{
+		{
+			TestName: "ID incorrect",
+			User: domain.User{
+				ID:       -1,
+				Nombre:   "Nombre",
+				Apellido: "Apellido",
+			},
+			Expected: ErrorInvalidID,
+		},
+		{
+			TestName: "Name incorrect",
+			User: domain.User{
+				ID:       1,
+				Nombre:   "",
+				Apellido: "Apellido",
+			},
+			Expected: ErrorNoName,
+		},
+		{
+			TestName: "Surname incorrect",
+			User: domain.User{
+				ID:       1,
+				Nombre:   "Nombre",
+				Apellido: "",
+			},
+			Expected: ErrorNoSurname,
+		},
+		{
+			TestName: "User not found",
+			User: domain.User{
+				ID:       2,
+				Nombre:   "Nombre",
+				Apellido: "Apellido",
+			},
+			Expected: ErrorUserNotFound,
+		},
 	}
-	err := validateBook(book)
-	if err == nil {
+
+	for _, c := range r {
+		t.Run(c.TestName, func(t *testing.T) {
+			_, err := UpdateUser(c.User)
+			if c.Expected != err {
+				t.Fail()
+			}
+		})
+	}
+}
+
+/* delete users */
+
+func TestDeleteUserOk(t *testing.T) {
+	ClearArrays()
+	CreateDataUser()
+
+	_, err := DeleteUser("1")
+	if err != nil {
 		t.Fail()
+	}
+}
+
+func TestDeleteUserNotOk(t *testing.T) {
+	ClearArrays()
+	CreateDataUser()
+
+	r := []struct {
+		TestName string
+		User     string
+		Expected error
+	}{
+		{
+			TestName: "ID incorrect 1",
+			User:     "assd",
+			Expected: ErrorInvalidID,
+		},
+		{
+			TestName: "User not found",
+			User:     "2",
+			Expected: ErrorUserNotFound,
+		},
+	}
+
+	for _, c := range r {
+		t.Run(c.TestName, func(t *testing.T) {
+			_, err := DeleteUser(c.User)
+			if c.Expected != err {
+				t.Fail()
+			}
+		})
+	}
+}
+
+/************************** TEST BOOKS ************************/
+
+func TestGetBookOk(t *testing.T) {
+	_, err := GetBook(domain.Book{
+		Titulo: "The Lord",
+		Autor:  "Tolkien",
+	})
+	if err != nil {
+		t.Fail()
+	}
+}
+
+func TestGetBookNotOk(t *testing.T) {
+	r := []struct {
+		TestName string
+		Book     domain.Book
+		Expected error
+	}{
+		{
+			TestName: "book special char",
+			Book: domain.Book{
+				Titulo: "The Lord",
+				Autor:  "Tol-?kien",
+			},
+			Expected: ErrorSpecialCharInBooks,
+		},
+		{
+			TestName: "book not found",
+			Book: domain.Book{
+				Titulo: "123n1dasmdp12",
+				Autor:  "zsouo23eo120",
+			},
+			Expected: ErrorBookNotFound,
+		},
+	}
+
+	for _, c := range r {
+		t.Run(c.TestName, func(t *testing.T) {
+			_, err := GetBook(c.Book)
+			if c.Expected != err {
+				t.Fail()
+			}
+		})
+	}
+}
+
+func TestApiResponseOk(t *testing.T) {
+	_, err := apiResponse(url)
+	if err != nil {
+		t.Fail()
+	}
+}
+
+func TestApiResponseNotOk(t *testing.T) {
+	r := []struct {
+		TestName string
+		Url      string
+		Expected error
+	}{
+		{
+			TestName: "book not found",
+			Url:      (based + "asd1231" + "+inauthor:" + "1231jb23kd1" + "&fields=items(id,volumeInfo(title,subtitle,authors,publishedDate))"),
+			Expected: ErrorBookNotFound,
+		},
+	}
+
+	for _, c := range r {
+		t.Run(c.TestName, func(t *testing.T) {
+			_, err := apiResponse(c.Url)
+			if c.Expected != err {
+				t.Fail()
+			}
+		})
 	}
 }
